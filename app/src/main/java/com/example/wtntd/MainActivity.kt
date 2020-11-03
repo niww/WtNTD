afafaa14bf422362d09a5dbd55faa69a22bdc5c4
niww <niww@ya.ru>
2020-11-03 20:03:52 +0300

    add sing in to app

--- app/src/main/java/com/example/wtntd/MainActivity.kt
+++ app/src/main/java/com/example/wtntd/MainActivity.kt
@@ -1,15 +1,10 @@
 package com.example.wtntd
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.whntd.data.NoteItemAdapter
-import com.google.android.gms.auth.api.signin.GoogleSignIn
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions
-import com.google.android.gms.common.api.ApiException
 
 class MainActivity : AppCompatActivity() {
     private lateinit var adapter: NoteItemAdapter
@@ -17,6 +12,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
 
         val recyclerView = findViewById<RecyclerView>(R.id.rv)
         val linearLayoutManager = LinearLayoutManager(this,LinearLayoutManager.VERTICAL, true)

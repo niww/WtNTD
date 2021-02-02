2eaa8a9465ac66475383c2f742a7d68ca8be7c30
niww <niww@ya.ru>
2021-02-02 22:21:46 +0300

    test dagger 2

--- app/src/main/java/com/example/wtntd/di/module/AppComponent.kt
+++ app/src/main/java/com/example/wtntd/di/module/AppComponent.kt
@@ -4,10 +4,12 @@ import com.example.wtntd.model.data.database.GetDBByLiveData
 import com.example.wtntd.model.data.database.IGetDataBase
 import com.example.wtntd.ui.activity.MainActivity
 import dagger.Component
+import javax.inject.Singleton
 
+@Singleton
 @Component(modules = [AppModule::class, AppModule::class,DataBaseModule::class,RoomModule::class])
 interface AppComponent {
     fun inject(mainActivity: MainActivity)
-    fun inject(getDBByLiveData: GetDBByLiveData)
+    fun inject(getDBByLiveData: IGetDataBase)
 
 }
